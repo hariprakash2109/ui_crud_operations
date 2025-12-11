a1c35ca2a5ed4cbe3e9bc2f70399235d4e0ca95c
Hari Prakash <hariprakash2109@gmail.com>
2025-12-11 16:20:02 +0530

    Remove backend/node_modules and delete unused src files

--- src/vdom.js
+++
@@ -1,8 +0,0 @@
-// src/vdom.js
-export function h(type, props = {}, ...children) {
-  props = props || {};
-  const flatChildren = children.flat().map(c => (typeof c === 'object' ? c : String(c)));
-  return { type, props, children: flatChildren };
-}
-
-export const Fragment = Symbol('Fragment');
